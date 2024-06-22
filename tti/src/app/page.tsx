2ffbe9a00482e92b1a0e415aadfae599c153fd8d
JangRuBin2 <wkdfnqls2465@gmail.com>
2024-06-22 15:00:56 +0900

    클라이언트 컴포넌트 선언하여 오류 제거 및 버튼이벤트 테스트

--- tti/src/app/page.tsx
+++ tti/src/app/page.tsx
@@ -1,10 +1,16 @@
+"use client";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const board_submit = () => {
+    console.log("test");
+  }
+
   return (
     <div className={styles.board_table}>
-      <input type="text" />
-      <input type="text" />
+      <input type="text" placeholder="title" id="board_title" className={styles.board_title}/>
+      <input type="text" placeholder="contents" id="board_contents" className={styles.board_contents}/>
+      <button type="button" onClick={board_submit}>저장</button>
     </div>
   );
 }
